54058a425348b668e4939bdb57625af7fbb61611
mohamedatia98 <mohamedatia039@gmail.com>
2025-10-20 09:40:37 +0300

    just some comments and adding jpa repository

--- RestCrudAPI/democrudapi/src/main/java/com/springbootapp/democrudapi/dao/EmployeeRepository.java
+++ RestCrudAPI/democrudapi/src/main/java/com/springbootapp/democrudapi/dao/EmployeeRepository.java
@@ -1,12 +1,16 @@
 package com.springbootapp.democrudapi.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.springbootapp.democrudapi.entity.Employee;
 
 // Entity Type -- Primary Key 
 // this will save much work of 70% code from entity manager work and 
 // creating interface and implentation files classes 
+
+//@RepositoryRestResource(path = "hola") // changing the default ( employees-name )
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
